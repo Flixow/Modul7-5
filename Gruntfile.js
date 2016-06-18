8de0957c898df5e4d4a80d39c64eb9dd55d40b0b
Daniel Siwek <danielsiwek95@gmail.com>
2016-06-18 15:09:58 +0200

    Configured grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,11 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    watch: {
+        files: '**/*.{sass,html}',
+        tasks: ['sass','cssmin']
+    },
+
 		sass: {
 		  options: {
 		    sourceMap: true
@@ -10,22 +15,59 @@ module.exports = function(grunt) {
 		      'style.css': 'style.sass'
 		    }
 		  }
-		}
+		},
 
 		imagemin: {
 		    dynamic: {
 		        files: [{
 		            expand: true,
-		            cwd: 'images/',
-		            src: ['**/*.{png,jpg,gif}'],
-		            dest: 'images/build/'
+		            cwd: './',
+		            src: ['**/*.{png,jpg,jpeg,gif}', '!node_modules/**', '!img/**'],
+		            dest: 'img'
 		        }]
 		    }
-		}
+		},
+
+		cssmin: {
+		  target: {
+		    files: [{
+		      expand: true,
+		      cwd: '',
+		      src: ['*.css', '!*.min.css'],
+		      dest: '',
+		      ext: '.min.css'
+		    }]
+		  }
+		},
+
+		clean: {
+		  folder: ['img','style.*','!style.sass']
+		},
+
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : [
+                    '*.css',
+                    '*.html'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: {
+									baseDir: "./"
+                }
+            }
+        }
+    }
   });
 
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-clean');
   
-  grunt.registerTask('default', ['sass','imagemin']);
+  grunt.registerTask('default', ['clean','sass','cssmin','imagemin','browserSync','watch']);
 };
